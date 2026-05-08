1ebca0f27a8aaac263d3b2f320b4338d9e3b7ab9
Abdallah Mohamed <pedrito1172341@gmail.com>
2026-05-08 19:03:20 +0300

    Change tabwdith

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -1,8 +1,8 @@
 vim.opt.cursorline = true
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.o.tabstop = 4
+vim.o.tabstop = 2
 vim.o.expandtab = true    
-vim.o.softtabstop = 4     
-vim.o.shiftwidth = 4      
+vim.o.softtabstop = 2     
+vim.o.shiftwidth = 2      
 vim.opt.clipboard = "unnamedplus"
